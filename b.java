a76c936f4fd0a0917ad659b1e77960464db2c662
ChenHang-GitHub <704635857@qq.com>
2019-08-04 13:40:10 +0800

    firstcommit

---
+++ b.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    /* 第一个Java程序
+     * 它将打印字符串 Hello World
+     */
+    public static void main(String []args) {
+ System.out.println("1"); 
+ System.out.println("2"); 
+ 
+        System.out.println("Hello World"); // 打印 Hello World
+    }
+}
